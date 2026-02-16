85fc8bb556216d4dd67a6b9074a6b69b5f083dcc
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 15:58:39 +0100

    Created Service Model / DTO for submission model.

---
+++ src/Acme.Core/DTOs/SubmissionDto.cs
@@ -0,0 +1,11 @@
+namespace Acme.Core.DTOs;
+
+public class SubmissionDto
+{
+    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? SerialNumber { get; set; }
+    public DateTime DateOfBirth { get; set; } 
+}
