17f6c20f689403a47a9c3e154af3deca0954bd8e
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 15:37:06 +0100

    Added data annotations to Submission model.

--- src/Acme.Core/Models/Submission.cs
+++ src/Acme.Core/Models/Submission.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Acme.Core.Models;
 
 public class Submission
 { 
+     
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string? FirstName { get; set; }
+        [Required, StringLength(50)]
         public string? LastName { get; set; }
+        [Required, EmailAddress]
         public string? Email { get; set; }
+        [Required, DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        
         public string? SerialCode { get; set; }
+        
         public DateTime SubmittedAt { get; set; }
 }
