df01a5d1a976fba4a50a3249b8fa836abfc186cf
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 21:57:42 +0100

    Removed data annotations from Submission model and added it to the SubmissionDto model instead.

--- src/Acme.Core/DTOs/SubmissionDto.cs
+++ src/Acme.Core/DTOs/SubmissionDto.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Acme.Core.DTOs;
 
 public class SubmissionDto
 {
-    
+
+    [Required, StringLength(50)]
     public string? FirstName { get; set; }
+    
+    [Required, StringLength(50)]
     public string? LastName { get; set; }
+    
+    [Required, EmailAddress]
     public string? Email { get; set; }
-    public string? SerialNumber { get; set; }
-    public DateTime DateOfBirth { get; set; } 
+    
+    [Required, DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+    
+    [Required]
+    public string? SerialNumber{ get; set; }
 }
--- src/Acme.Core/Models/Submission.cs
+++ src/Acme.Core/Models/Submission.cs
@@ -6,13 +6,13 @@ public class Submission
 { 
      
         public int Id { get; set; }
-        [Required, StringLength(50)]
+
         public string? FirstName { get; set; }
-        [Required, StringLength(50)]
+       
         public string? LastName { get; set; }
-        [Required, EmailAddress]
+      
         public string? Email { get; set; }
-        [Required, DataType(DataType.Date)]
+     
         public DateTime BirthDate { get; set; }
         
         public string? SerialCode { get; set; }
