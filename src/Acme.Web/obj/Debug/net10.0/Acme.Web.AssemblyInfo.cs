fca2e38b32fc90e4d23f84007e596245a955839d
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 22:58:51 +0100

    Added navbar, and created pages for home and blog.

--- src/Acme.Web/obj/Debug/net10.0/Acme.Web.AssemblyInfo.cs
+++ src/Acme.Web/obj/Debug/net10.0/Acme.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Acme.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab9aa0dabe0d5ae36c8ba1b8ceda94519ed60ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d6f70bfd15735b6bfac1f46d30479562852e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Acme.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Acme.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
