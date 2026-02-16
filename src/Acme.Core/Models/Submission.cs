f60f1f67671446d99a973fa33ff10e06c6e21017
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 15:29:28 +0100

    Submission model.

---
+++ src/Acme.Core/Models/Submission.cs
@@ -0,0 +1,12 @@
+namespace Acme.Core.Models;
+
+public class Submission
+{ 
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? SerialCode { get; set; }
+        public DateTime SubmittedAt { get; set; }
+}
