65b1b675ce6769502d73ab2b4790a2088c9d0ec7
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-16 22:08:40 +0100

    Added logic to SubmissionService.cs Will now return false if contraints isnt met. Also made method for calculating if the person is 18 years of age.

--- src/Acme.Core/Services/SubmissionService.cs
+++ src/Acme.Core/Services/SubmissionService.cs
@@ -1,12 +1,64 @@
+using Acme.Core.Data;
 using Acme.Core.DTOs;
 using Acme.Core.Interfaces;
+using Acme.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Acme.Core.Services;
 
 public class SubmissionService : ISubmissionService
 {
-    public Task<(bool Success, string? Error)> SubmitAsync(SubmissionDto dto)
+    public async Task<(bool Success, string? Error)> SubmitAsync(SubmissionDto dto)
     {
-        throw new NotImplementedException();
+        if (!IsAtLeast18(dto.DateOfBirth))
+            return (false, "You must be at least 18 years old.");
+
+        var serialExists = await _dbContext.SerialNumbers
+            .AnyAsync(s => s.Code == dto.SerialNumber);
+
+        if (!serialExists)
+            return (false,"Invalid serial number.");
+
+        var count = await _dbContext.Submissions
+            .CountAsync(s => s.SerialCode == dto.SerialNumber);
+
+        if (count >= 2)
+            return (false,"Serial number already used twice.");
+
+        var submission = new Submission
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Email = dto.Email,
+            SerialCode = dto.SerialNumber,
+            BirthDate = dto.DateOfBirth,
+            SubmittedAt = DateTime.UtcNow
+        };
+
+        _dbContext.Submissions.Add(submission);
+        await _dbContext.SaveChangesAsync();
+
+        return (true,null);
+    }
+    
+    private bool IsAtLeast18(DateTime dob)
+    {
+        var today = DateTime.Today;
+        var age = today.Year - dob.Year;
+
+        var birthdayThisYear = dob.AddYears(age);
+
+        if (birthdayThisYear > today) // Check if they had birthday already.
+        {
+            age--;
+        }
+        return age >= 18;
+    }
+
+    private readonly AcmeDbContext _dbContext;
+
+    public SubmissionService(AcmeDbContext dbContext)
+    {
+        _dbContext = dbContext;
     }
 }
