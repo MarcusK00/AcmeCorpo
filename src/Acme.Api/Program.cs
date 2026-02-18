81810e91b708286d75153821deab2d26493153da
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-18 15:10:25 +0100

    Added SubmissionServices and ApiServices to middleware in Acme.Api/program.cs so it can be DI. Also added AddControllers to builder.services & CORS

--- src/Acme.Api/Program.cs
+++ src/Acme.Api/Program.cs
@@ -1,8 +1,32 @@
+using Acme.Core.Data;
+using Acme.Core.Interfaces;
+using Acme.Core.Persistence;
+using Acme.Core.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddControllers();
+
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<ISubmissionRepository, SubmissionRepository>();
+builder.Services.AddScoped<ISubmissionService, SubmissionService>();
+
+builder.Services.AddDbContext<AcmeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:5056", "https://localhost:5056")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -13,5 +37,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ✅ Use CORS
+app.UseCors("AllowClient");
+
+app.UseAuthorization();
+
+// ✅ Map Controllers - THIS WAS MISSING!
+app.MapControllers();
 
 app.Run();
