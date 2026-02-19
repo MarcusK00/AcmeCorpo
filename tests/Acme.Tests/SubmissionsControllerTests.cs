1da0b9d73eb458fcb12abfbee84ddbae58e897fb
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-19 10:48:17 +0100

    Added a new method for testing GetAll method in SubmissionsController.cs

--- tests/Acme.Tests/SubmissionsControllerTests.cs
+++ tests/Acme.Tests/SubmissionsControllerTests.cs
@@ -39,8 +39,41 @@ public class SubmissionControllerTests
         Assert.IsInstanceOfType(result, typeof(OkResult));
         mockService.Verify(s => s.SubmitAsync(It.IsAny<SubmissionDto>()), Times.Once);
     }
-    
-    
+
+    [TestMethod]
+    public async Task GetAll_ReturnsCorrectList_ReturnsOk()
+    {
+        //Arrange
+        var mockService = new Mock<ISubmissionService>();
+        var testController = new SubmissionsController(mockService.Object);
+        var testSubmissions = new List<Submission>()
+        {
+            new Submission
+            {
+                Id = 1, FirstName = "Kristian", LastName = "Helse", Email = "kh@mail.com",
+                BirthDate = new DateTime(2000, 03, 12), SerialCode = "THIS-IS-TEST-SERIAL",
+                SubmittedAt = DateTime.UtcNow
+            },
+            new Submission
+            {
+                Id = 2, FirstName = "Hans", LastName = "Erik", Email = "he@mymail.co",
+                BirthDate = new DateTime(1997, 05, 20), SerialCode = "THIS-IS-MY-SERIAL", SubmittedAt = DateTime.UtcNow
+            },
+        };
+        
+        mockService.Setup(s => s.GetAllSubmissionsAsync()).ReturnsAsync(testSubmissions);
+        
+        //Act
+        var result = await testController.GetAllAsync();
+        var okResult = result.Result as OkObjectResult; 
+        var dtos = okResult!.Value as List<SubmissionDto>;
+        
+        //Assert
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+        Assert.HasCount(2,dtos!);
+        mockService.Verify(s=>s.GetAllSubmissionsAsync(), Times.Once);
+    }
 
 
     
