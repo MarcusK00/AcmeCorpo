3f967f18deee27ef806efd5f3d49e67c4801049b
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-19 11:35:27 +0100

    Changed data annotations in SubDto. Also made DateTime nullable.

--- src/Acme.Shared/DTOs/SubmissionDto.cs
+++ src/Acme.Shared/DTOs/SubmissionDto.cs
@@ -4,19 +4,21 @@ namespace Acme.Shared.DTOs;
 
 public class SubmissionDto
 {
-
-    [Required, StringLength(50)]
-    public string? FirstName { get; set; }
+    [Required(ErrorMessage = "First name is required")]
+    [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+    public string FirstName { get; set; } = string.Empty;
     
-    [Required, StringLength(50)]
-    public string? LastName { get; set; }
+    [Required(ErrorMessage = "Last name is required")]
+    [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+    public string LastName { get; set; } = string.Empty;
     
-    [Required, EmailAddress]
-    public string? Email { get; set; }
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = string.Empty;
     
-    [Required, DataType(DataType.Date)]
-    public DateTime DateOfBirth { get; set; }
+    [Required(ErrorMessage = "Date of birth is required")]
+    public DateTime? DateOfBirth { get; set; }
     
-    [Required]
-    public string? SerialNumber{ get; set; }
+    [Required(ErrorMessage = "Serial number is required")]
+    public string SerialNumber { get; set; } = string.Empty;
 }
