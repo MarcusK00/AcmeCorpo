04966cdb02e3f510610b8e50a04a63e2c18883ec
MarcusK00 <marcuskristensen10@hotmail.com>
2026-02-19 10:01:45 +0100

    Add .gitignore and remove build artifacts

---
+++ tests/Acme.Tests/SubmissionServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Acme.Core.Interfaces;
+using Acme.Core.Models;
+using Acme.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acme.Tests;
+
+[TestClass]
+public class SubmissionServiceTests
+{
+
+    [TestMethod]
+    public async Task GetAllSubmissions_ReturnsAllSubmissions()
+    {
+        // Arrange
+        var mockRepository = new Mock<ISubmissionRepository>();
+        var testSubmissions = new List<Submission>
+        {
+            new Submission { Id = 1, FirstName = "Kristian", LastName ="Herlev", Email = "kh@mail.com", BirthDate = new DateTime(2000,03,12), SerialCode = "TEST-TEST-TEST-TEST", SubmittedAt = DateTime.UtcNow},
+            new Submission { Id = 2, FirstName = "Hans", LastName ="Erik", Email = "he@mymail.co", BirthDate = new DateTime(1997,05,20), SerialCode = "THIS-IS-MY-SERIAL", SubmittedAt = DateTime.UtcNow},
+        };
+        mockRepository.Setup(repo => repo.GetAllSubmissionsAsync()).ReturnsAsync(testSubmissions);
+
+        var service = new SubmissionService(mockRepository.Object);
+
+        // Act
+        var result = await service.GetAllSubmissionsAsync();
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count());
+        mockRepository.Verify(repo => repo.GetAllAsync(), Times.Once);
+    }
+    
+}
